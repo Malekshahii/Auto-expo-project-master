5d804fc91e243d20d9cf853305d7f6ac292f4926
Amir3720 <132751736+Amir3720@users.noreply.github.com>
2023-05-13 00:52:45 +0330

    type load page event

--- script/script.js
+++ script/script.js
@@ -38,3 +38,19 @@ TxtType.prototype.tick = function() {
     that.tick();
     }, delta);
 };
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    document.body.appendChild(css);
+};
+
