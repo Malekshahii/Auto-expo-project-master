64dbe6e9a62454f7d9794be816ef4c55cee7251f
Amir3720 <132751736+Amir3720@users.noreply.github.com>
2023-05-13 00:53:17 +0330

    Update script slider js code

--- script/script.js
+++ script/script.js
@@ -54,3 +54,59 @@ window.onload = function() {
     document.body.appendChild(css);
 };
 
+
+// اسلایدر
+
+var slides=document.getElementsByClassName("slide");
+var prev=document.querySelector(".prev");
+var next=document.querySelector(".next");
+var n=0;
+
+function displayNone(){
+    for(let i=0;i<slides.length;i++){
+        slides[i].style.display='none';
+    }
+}
+
+function noActive(){
+    for(let i=0;i<slides.length;i++){
+        slides[i].classList.remove('active');
+
+    }
+
+}
+
+prev.addEventListener("click",function(){
+    n++;
+    if(n>slides.length-1){
+        n=0;
+    }
+    displayNone();
+    noActive();
+    slides[n].style.display='block';
+    slides[n].classList.add('active');
+
+});
+
+next.addEventListener("click",function(){
+    n--;
+    if(n<0){
+        n=slides.length-1;
+    }
+    displayNone();
+    noActive();
+    slides[n].style.display='block';
+    slides[n].classList.add('active');
+
+});
+
+setInterval(function(){
+    n++;
+    if(n>slides.length-1){
+        n=0;
+    }
+    displayNone();
+    noActive();
+    slides[n].style.display='block';
+    slides[n].classList.add('active');
+},6000)
